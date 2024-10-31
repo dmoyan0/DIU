cb41edab9303e23ca09b022067d7a0ab2cfdc833
dmoyan0 <diego.moyano@usm.cl>
2024-10-31 16:21:40 -0300

    Cambios en Agregar Semanas y Header

--- menu-familiar/src/components/Header.js
+++ menu-familiar/src/components/Header.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+
 
 function Header() {
   return (
     <header className="header">
-      <div className="logo">Menú Familiar</div>
+
+      {/* // <div className="logo">Menú Familiar</div> */}
+      
+      <Link to="/" className = "logo">
+        Menú Familiar
+      </Link>
+
       <nav>
-        <a href="/">Home</a>
-        <a href="/">Seleccionar menú</a>
-        <a href="/">Acerca</a>
-        <a href="/">Iniciar sesión</a>
-        <a href="/">Registrarse</a>
+        <Link to="/">Home</Link>
+        <Link to="/menu">Seleccionar menú</Link>
+        <Link to="/">Acerca</Link>
+        <Link to="/">Iniciar sesión</Link>
+        <Link to="/">Registrarse</Link>
       </nav>
       <div className="social-icons">
         <i className="fab fa-facebook"></i>
--- menu-familiar/src/components/MenuView.js
+++ menu-familiar/src/components/MenuView.js
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './MenuView.css';
 
 function MenuView() {
+  // Para mantener track de las semanas:
+  const [semanas, setSemanas] = useState([{id: 1, name: "Semana 1"}]);
+
+  const maxWeeks = 8;
+  const weeksPerColumn = 4;
+
+  // Funcion para agregar una semana:
+  const AgregarSemana = () => {
+    const newWeekNumber = semanas.length + 1;
+    const newSemana = {id: semanas.length + 1, name: ` Semana ${newWeekNumber}`};
+    setSemanas([...semanas, newSemana]);
+  };
+
+  // Funcion para eliminar una semana en especifico
+  const eliminarSemana = (id) => {
+    setSemanas(semanas.filter((semana) => semana.id != id));
+  };
+
+  const chunkWeeks = (weeks, size) => {
+    const result = [];
+    for (let i = 0; i < weeks.length; i += size) {
+      result.push(weeks.slice(i, i + size));
+    }
+    return result;
+  };
+
+  const weekColumns = chunkWeeks(semanas, weeksPerColumn);
+
   return (
     <div className="menu-view">
-      <button className="add-week-button">+Agregar Semana</button>
+      {semanas.length < maxWeeks && (
+        <button className="add-week-button" onClick={AgregarSemana}>
+        +Agregar Semana
+        </button>
+      )}
       
-      <div className="week">
-        <span>Semana 1</span>
-        <button className="add-menu-button">+Agregar menú</button>
+      
+      <div className="week-container">
+        {weekColumns.map((column, columnIndex) => (
+          <div key={columnIndex} className="weekColumn"> 
+          {column.map((semana) => (
+            <div key={semana.id} className='week'>
+              <span>{semana.name}</span>
+              <button className='add-menu-button'>+Agregar menú</button>
+              <button className='delete-week-button' onClick={() => eliminarSemana(semana.id)}>
+                Eliminar
+              </button>
+            </div>
+          ))}
+          </div>
+        ))}
       </div>
       
-      <button className="shopping-list-button">Obtener lista de compras</button>
+      {semanas.length != 0 && (
+        <button className="shopping-list-button">Obtener lista de compras</button>
+      )}
+
     </div>
   );
 }
